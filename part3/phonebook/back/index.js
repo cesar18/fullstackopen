013811e25974180fd5949724f0c603f270be50e0
Cesar18 <cesarhenriquecicero@gmail.com>
2025-11-13 16:31:01 -0300

    uibuild

--- part3/phonebook/back/index.js
+++ part3/phonebook/back/index.js
@@ -1,64 +1,68 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
+// if no root, execute frontend
+// dist is folder where vite build all files
+// transpiling, minimaxing and merging files
 app.use(express.static('dist'))
 
+// defines the structure of body passed in require
 app.use(express.json())
 
+// specify output of morgan
 morgan.token('type', (req, res) => { 
     return JSON.stringify(req.body)
 })
-
+// uses are sequencial as they appear on index
+// first we must standerysed the body before showing
+// requestLogger*
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/info', (request, response) => {
     const time = new Date
-    response.send(`<p>
+    Person
+    .find({})
+    .then(persons=>{
+      response
+      .send(`<p>
         Phonebook has info for ${persons.length} people<br/>
         ${time.toString()}
-        </p>`)
+        </p>`
+      )
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person
+    .find({})
+    .then(persons=>{
+      response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const person = persons.find(person=> person.id === request.params.id)
-    if(person){
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(person=>{
+      if(person){
         response.json(person)
-    }else{
+      }else{
         response.statusMessage = "Person does not exists"
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error=>next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    persons = persons.filter(person => person.id !== request.params.id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result=>{
+        response.status(204).end()
+      })
+      .catch(error=>next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -71,30 +75,73 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if(persons.find(person=>person.name === body.name)){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
+  Person.findOne({name: body.name})
+    .then(person=>{
+      if(!person){
+        person = new Person({
+          name: body.name,
+          number: body.number
+        })
+      }else{
+        person.number = body.number
+      }
+
+      person
+        .save()
+        .then(personSaved=>{
+          response.json(personSaved)
+        })
     })
-  }
-
-  const person = {
-    id: `${Math.floor(Math.random()*10000)}`,
-    name: body.name,
-    number: body.number
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name, number} = request.body
+
+  Person
+    .findById(request.params.id)
+    .then(person=>{
+      if(!person){
+        return response.status(400).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person
+        .save()
+        .then((updatedPerson)=> {
+          response.json(updatedPerson)
+        })
+    })
+    .catch(error=>next(error))
+})
+
+// defines a route of page not found
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
+// if none route is ispecified, goes to page 404 (Not Found)
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+
+// route to define when error ocours
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+// starts listenning (at the end of file)
+// process.env.PORT is default port of Render
+// if not in Render (not defined), use port 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebook/back/models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+// { family: 4 } is to use IP4
+mongoose.connect(url, { family: 4 })
+    .then(result=>{
+        console.log('connected to MongoDB')
+    })
+    .catch(error=>{
+        console.log('error connecting to MongoDB: ',error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
