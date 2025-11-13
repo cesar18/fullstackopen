b2211ad271001614f4d44952073c1d7dc5fbdbb1
Cesar18 <cesarhenriquecicero@gmail.com>
2025-11-13 14:18:03 -0300

    part3.12 ok mongose

---
+++ part3/phonebook/back/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://cesar_db_user:${password}@cluster0.hdg0833.mongodb.net/phoneApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+// { family: 4 } is to use IP4
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+if(process.argv[4]){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+      mongoose.connection.close()
+    })
+}else{
+    Person
+    .find({})
+    .then(persons => {
+        console.log('phonebook:')
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        //must close conection inside then or catch
+        mongoose.connection.close()
+    })
+}
